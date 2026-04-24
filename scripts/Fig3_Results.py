dca04fbf64ed69c370c1c51e0d16e8e4b0109244
duankuxiao <duankuxiao@gmail.com>
2026-04-24 15:34:59 +0800

    initial clean repository import

--- scripts/Fig3_Results.py
+++ scripts/Fig3_Results.py
@@ -16,27 +16,26 @@ if __name__ == '__main__':
                  "Poland", "Switzerland"]
     scenarios = ["Base"]
     renewable_energy_policy = 'CP'
-    LLM_policy = 'SDS'
-    if LLM_policy == 'SDS':  # Strict Data Sovereignty
-        infer_ratio_by_country = {'USA': 0.7, 'Belgium': 0.7}
-    elif LLM_policy == 'UGD':  # Unconstrained Global Deployment
-        infer_ratio_by_country = {'USA': 0.6106, 'Belgium': 0.0000, 'France': 0.0000, 'Sweden': 0.0000, 'Spain': 0.0000, 'Netherlands': 0.0000, 'Germany': 0.0000, 'Italy': 0.0000,
-                                  'Poland': 1.0000, 'Ireland': 0.0000, 'Norway': 1.0000, 'Israel': 1.0000, 'Brazil': 1.0000, 'UAE': 1.0000, 'South_Korea': 0.0000,
-                                  'Australia': 1.0000,'United_Kingdom': 0.0000, 'Canada': 1.0000, 'Singapore': 0.0000, 'India': 1.0000, 'Japan': 1.0000, 'China': 1.0000,
-                                  'South_Africa': 1.0000, 'Switzerland': 1.0000}
-    elif LLM_policy == 'BCD':  # Bloc-Constrained Deployment
-        infer_ratio_by_country = {"USA": 0.7000, "Belgium": 0.0000, "France": 0.3575, "Sweden": 0.0000, "Spain": 0.0000, "Netherlands": 1.0000, "Germany": 1.0000, "Italy": 1.0000,
-                                  "Poland": 1.0000, "Ireland": 1.0000, "Norway": 1.0000, "Israel": 0.0000, "Brazil": 1.0000, "UAE": 1.0000, "South_Korea": 0.0000,
-                                  "Australia": 1.0000, "United_Kingdom": 0.0000, "Canada": 1.0000, "Singapore": 0.0000, "India": 1.0000, "Japan": 1.0000, "China": 0.6852,
-                                  "South_Africa": 1.0000, "Switzerland": 0.0000, }
-
-    scenarios = ["Base"]
-
-    AIFootprint(renewable_energy_policy,
-                scenarios,
-                years,
-                countries,
-                infer_ratio_by_country=infer_ratio_by_country,
-                default_p_infer=0.7,
-                output_dir=str(ROOT_DIR / "results" / "fig3" / LLM_policy),
-                year_start=2025)
+    for LLM_policy in ['SDS', 'UGD', 'CP']:
+        if LLM_policy == 'SDS':  # Strict Data Sovereignty
+            infer_ratio_by_country = {'USA': 0.7, 'Belgium': 0.7}
+        elif LLM_policy == 'UGD':  # Unconstrained Global Deployment
+            infer_ratio_by_country = {'USA': 0.6106, 'Belgium': 0.0000, 'France': 0.0000, 'Sweden': 0.0000, 'Spain': 0.0000, 'Netherlands': 0.0000, 'Germany': 0.0000,
+                                      'Italy': 0.0000,
+                                      'Poland': 1.0000, 'Ireland': 0.0000, 'Norway': 1.0000, 'Israel': 1.0000, 'Brazil': 1.0000, 'UAE': 1.0000, 'South_Korea': 0.0000,
+                                      'Australia': 1.0000, 'United_Kingdom': 0.0000, 'Canada': 1.0000, 'Singapore': 0.0000, 'India': 1.0000, 'Japan': 1.0000, 'China': 1.0000,
+                                      'South_Africa': 1.0000, 'Switzerland': 1.0000}
+        elif LLM_policy == 'BCD':  # Bloc-Constrained Deployment
+            infer_ratio_by_country = {"USA": 0.7000, "Belgium": 0.0000, "France": 0.3575, "Sweden": 0.0000, "Spain": 0.0000, "Netherlands": 1.0000, "Germany": 1.0000,
+                                      "Italy": 1.0000,
+                                      "Poland": 1.0000, "Ireland": 1.0000, "Norway": 1.0000, "Israel": 0.0000, "Brazil": 1.0000, "UAE": 1.0000, "South_Korea": 0.0000,
+                                      "Australia": 1.0000, "United_Kingdom": 0.0000, "Canada": 1.0000, "Singapore": 0.0000, "India": 1.0000, "Japan": 1.0000, "China": 0.6852,
+                                      "South_Africa": 1.0000, "Switzerland": 0.0000, }
+        AIFootprint(renewable_energy_policy,
+                    scenarios,
+                    years,
+                    countries,
+                    infer_ratio_by_country=infer_ratio_by_country,
+                    default_p_infer=0.7,
+                    output_dir=str(ROOT_DIR / "results" / "fig3" / LLM_policy),
+                    year_start=2025)
